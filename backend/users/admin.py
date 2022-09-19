a78459a368248c697b8c3d1bed1dc4e064039895
Denis Mushits <horo-braven@yandex.ru>
2022-09-19 22:11:45 +0300

    users.admin, users.apps

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import Follow, User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+    list_filter = ('username', 'first_name', 'email',)
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'author',)
+    list_filter = ('user', 'author',)
