f8e454acf8cc8e99200ec4aa3a57912238fb0157
Denis Mushits <horo-braven@yandex.ru>
2022-09-19 21:06:13 +0300

    back to initial

--- backend/api/mixins.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import mixins, viewsets
-
-
-class CreateDestroyViewSet(mixins.CreateModelMixin,
-                           mixins.DestroyModelMixin,
-                           viewsets.GenericViewSet):
-    pass
