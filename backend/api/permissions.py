69591176992b3e942768db7cedaec050439240f8
Denis Mushits <horo-braven@yandex.ru>
2022-09-24 16:48:41 +0300

    api.permissions

---
+++ backend/api/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS or request.user.is_staff)
+
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS or request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
